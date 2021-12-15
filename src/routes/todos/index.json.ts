fe44a374115178a0a7d9ef6901a5bd520a08b747
Niko Mendo <nicolasmendonca@gmail.com>
2021-12-15 03:17:25 +0000

    Adds todo listing and creation

--- src/routes/todos/index.json.ts
+++ src/routes/todos/index.json.ts
@@ -1,20 +1,27 @@
 import type { RequestHandler } from '@sveltejs/kit';
+import PrismaClient from '$lib/prisma';
+
+const prisma = new PrismaClient()
 
 export const get: RequestHandler = async () => {
   return {
     status: 200,
-    body: []
+    body: await prisma.todo.findMany()
   }
 }
 
 export const post: RequestHandler<unknown, FormData> = async (request) => {
   const text = request.body.get('text')
 
+  const todo = await prisma.todo.create({
+    data: {
+      text,
+      done: false,
+    }
+  })
+
   return {
     status: 201,
-    body: {
-      id: 1,
-      text,
-    }
+    body: todo
   }
 }
